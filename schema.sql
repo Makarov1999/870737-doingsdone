f64c8a430f7c62cbc4e3f29c10af87596a006921
Makarov1999 <makarov.e1999@yandex.ru>
2019-02-10 21:58:14 +0300

    Database added

--- schema.sql
+++ schema.sql
@@ -20,6 +20,21 @@ CREATE TABLE cite_user (
 	username CHAR(40),
 	password TEXT
 );	
+ALTER TABLE task ADD task_file VARCHAR(50);
 
+CREATE UNIQUE INDEX id_project ON project(project_id);
+CREATE INDEX name_project ON project(project_name);
+CREATE UNIQUE INDEX id_task ON task(task_id);
+CREATE INDEX create_date ON task(date_create);
+CREATE INDEX complete_date ON task(date_complete);
+CREATE INDEX status_task ON task(task_status);*/
+CREATE INDEX name_task ON task(task_name(40));
+CREATE INDEX dedl ON task(deadline);
+CREATE INDEX file_task ON task(task_file);
+CREATE UNIQUE INDEX id_user ON cite_user(user_id);
+CREATE INDEX date_registration ON cite_user(registration_date);
+CREATE INDEX user_email ON cite_user(email);
+CREATE INDEX name ON cite_user(username);
+CREATE INDEX passw ON cite_user(password(100));
 
 
