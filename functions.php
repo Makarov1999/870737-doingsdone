52b0a79e98ce93ab5f1c8171cd5c76c3ff925008
Makarov1999 <makarov.e1999@yandex.ru>
2019-02-17 23:53:35 +0300

    m

--- functions.php
+++ functions.php
@@ -22,4 +22,38 @@ function count_tasks( $tasks_array, $project_key) {
   }
   return $quantity;
 }
+function db_get_prepare_stmt($link, $sql, $data = []) {
+    $stmt = mysqli_prepare($link, $sql);
+
+    if ($data) {
+        $types = '';
+        $stmt_data = [];
+
+        foreach ($data as $value) {
+            $type = null;
+
+            if (is_int($value)) {
+                $type = 'i';
+            }
+            else if (is_string($value)) {
+                $type = 's';
+            }
+            else if (is_double($value)) {
+                $type = 'd';
+            }
+
+            if ($type) {
+                $types .= $type;
+                $stmt_data[] = $value;
+            }
+        }
+
+        $values = array_merge([$stmt, $types], $stmt_data);
+
+        $func = 'mysqli_stmt_bind_param';
+        $func(...$values);
+    }
+
+    return $stmt;
+}
 ?>
