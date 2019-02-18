d2f3f91e6d882d8dea148a7330a7607173a68972
Makarov1999 <makarov.e1999@yandex.ru>
2019-02-18 21:50:56 +0300

    Work with request parameters

---
+++ templates/error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ошибка 404</title>
+  </head>
+  <body>
+    <?php
+    echo "Ошибка 404";
+     ?>
+  </body>
+</html>
