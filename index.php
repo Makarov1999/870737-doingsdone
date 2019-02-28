713b8901b026fc170a300a519a0e5fe229315a58
Makarov1999 <makarov.e1999@yandex.ru>
2019-02-28 23:31:20 +0300

    Add autorisation

--- index.php
+++ index.php
@@ -1,15 +1,33 @@
 <?php
 require_once('functions.php');
 require_once('init.php');
+session_start();
 
-
-$projects = get_projects($con, $arr);
-if (isset($_GET['project_id'])) {
-  if (!(consist_array($_GET['project_id'], $projects))) {
-    header("Location: 404.php");
+if (isset($_SESSION['user'])) {
+  $user_id = intval($_SESSION['user']);
+  $user = get_user_data($con, $user_id);
+  $arr = [$user_id];
+  $projects = get_projects($con, $arr);
+  if (isset($_GET['show_completed'])) {
+    $show_complete_tasks = intval($_GET['show_completed']);
+  }
+  if (isset($_GET['project_id'])) {
+    if (!(consist_array($_GET['project_id'], $projects))) {
+      header("Location: 404.php");
+    } else {
+      $arr[] = $_GET['project_id'];
+      $sql2 = "SELECT t.task_name, t.deadline, t.project_id, t.task_status FROM task t INNER JOIN cite_user c ON t.id_user = c.user_id AND t.id_user = ? AND t.project_id = ?";
+      $stmt2 = db_get_prepare_stmt($con,$sql2,$arr);
+      mysqli_stmt_execute($stmt2);
+      $result_tasks = mysqli_stmt_get_result($stmt2);
+      mysqli_stmt_close($stmt2);
+      $tasks = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
+      $main_page_content = include_template('index.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
+      $layout_content = include_template(
+        'layout.php', ['content' => $main_page_content, 'title' => 'Дела в порядке Главная', 'con' => $con, 'projects' => $projects, 'user' => $user]);
+    }
   } else {
-    $arr[] = $_GET['project_id'];
-    $sql2 = "SELECT t.task_name, t.deadline, t.project_id, t.task_status FROM task t INNER JOIN cite_user c ON t.id_user = c.user_id AND t.id_user = ? AND t.project_id = ?";
+    $sql2 = "SELECT * FROM task WHERE id_user = ?";
     $stmt2 = db_get_prepare_stmt($con,$sql2,$arr);
     mysqli_stmt_execute($stmt2);
     $result_tasks = mysqli_stmt_get_result($stmt2);
@@ -17,37 +35,11 @@ if (isset($_GET['project_id'])) {
     $tasks = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
     $main_page_content = include_template('index.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
     $layout_content = include_template(
-      'layout.php', ['content' => $main_page_content, 'title' => 'Дела в порядке Главная', 'con' => $con, 'projects' => $projects]);
-    print($layout_content);
+      'layout.php', ['content' => $main_page_content, 'title' => 'Дела в порядке Главная', 'con' => $con, 'projects' => $projects, 'user' => $user]);
   }
 } else {
-  $sql2 = "SELECT t.task_name, t.deadline, t.project_id, t.task_status FROM task t INNER JOIN cite_user c ON t.id_user = c.user_id AND t.id_user = ?";
-  $stmt2 = db_get_prepare_stmt($con,$sql2,$arr);
-  mysqli_stmt_execute($stmt2);
-  $result_tasks = mysqli_stmt_get_result($stmt2);
-  mysqli_stmt_close($stmt2);
-  $tasks = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
-  $main_page_content = include_template('index.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
-  $layout_content = include_template(
-    'layout.php', ['content' => $main_page_content, 'title' => 'Дела в порядке Главная', 'con' => $con, 'projects' => $projects]);
-  print($layout_content);
+  $content = include_template('guest.php', []);
+  $layout_content = include_template('layout.php', ['content' => $content, 'is_sidebar' => 1]);
 }
-
-/*if (isset($_GET['project_id'])) {
-
-  $arr[] = $_GET['project_id'];
-  $sql2 = "SELECT t.task_name, t.deadline, t.project_id, t.task_status FROM task t INNER JOIN cite_user c ON t.id_user = c.user_id AND t.id_user = ? AND t.project_id = ?";
-} else {
-  $sql2 = "SELECT t.task_name, t.deadline, t.project_id, t.task_status FROM task t INNER JOIN cite_user c ON t.id_user = c.user_id AND t.id_user = ?";
-}
-$stmt2 = db_get_prepare_stmt($con,$sql2,$arr);
-mysqli_stmt_execute($stmt2);
-$result_tasks = mysqli_stmt_get_result($stmt2);
-mysqli_stmt_close($stmt2);
-$tasks = mysqli_fetch_all($result_tasks, MYSQLI_ASSOC);
-$main_page_content = include_template('index.php', ['tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
-$layout_content = include_template(
-  'layout.php', ['content' => $main_page_content, 'title' => 'Дела в порядке Главная', 'tasks' => $tasks, 'projects' => $projects]);
-print($layout_content);*/
-
+print($layout_content);
 ?>
---
+++ project.php
@@ -0,0 +1,36 @@
+<?php
+require_once('init.php');
+require_once('functions.php');
+session_start();
+if(isset($_SESSION['user'])) {
+  $user_id = intval($_SESSION['user']);
+  $user = get_user_data($con, $user_id);
+  $title = 'Добавление проекта';
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $project_name = htmlspecialchars($_POST['name']);
+    $errors = [];
+    if (empty($project_name)) {
+      $errors['name'] = 'Введите название проекта';
+    } else {
+      if (is_project_exist($con, $project_name, $user_id)) {
+        $errors['name'] = 'Данный проект существует';
+      }
+    }
+    if (empty($errors)) {
+      $sql = 'INSERT INTO project (project_name, id_user) VALUES (?, ?)';
+      $stmt = db_get_prepare_stmt($con, $sql, [$project_name, $user_id]);
+      mysqli_stmt_execute($stmt);
+      header('Location: index.php');
+    } else {
+      $content = include_template('project.php', ['errors' => $errors]);
+    }
+  } else {
+    $content = include_template('project.php', []);
+  }
+  $layout_content = include_template('layout.php', ['content' => $content,'projects' => $projects, 'title' => $title, 'user' => $user]);
+  print($layout_content);
+} else {
+  header('Location: index.php');
+}
+
+?>
